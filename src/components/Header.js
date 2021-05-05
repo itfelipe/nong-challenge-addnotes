8408a7059d3db0bc90cd8cbeb1895f37f09603d5
Felipe Rocha <74837527+itfelipe@users.noreply.github.com>
2021-05-05 13:01:40 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = props => (
     <header className="header">
-        <h3 className="title">Crop <span>Scouting </span>Notes</h3>
+        <h3 className="title">Crop <span>nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn </span>Notes</h3>
 
     </header>
 );
